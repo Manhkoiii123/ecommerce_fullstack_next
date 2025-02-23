b2fa42786f93816560d24effbd0a0d4918f922c9
manhtranduc <manhtranduc0202@gmail.com>
2025-02-23 12:08:24 +0700

    add clerk

---
+++ docs.md
@@ -0,0 +1,3 @@
+# setup prisma
+
+`npm i -D prisma` và `@prisma/client` => `npx prisma init` => tạo schema => `npx prisma generate` và `npx prisma db push`
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import ThemeToggle from "@/components/shared/theme-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <>
-      <ThemeToggle />
-    </>
+    <div className="p-5">
+      <div className="w-100 flex justify-end gap-x-5">
+        <UserButton />
+        <ThemeToggle />
+      </div>
+    </div>
   );
 }
