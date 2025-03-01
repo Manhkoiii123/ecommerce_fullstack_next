87f6c12f6bb4fccd7934bbbbbfc1bb8a310f2b75
manhtranduc <manhtranduc0202@gmail.com>
2025-03-01 11:15:02 +0700

    add countries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import ThemeToggle from "@/components/shared/theme-toggle";
-import { updateVariantImage } from "@/migration-scripts/migrate-varinatImage";
 import { UserButton } from "@clerk/nextjs";
 
 export default async function Home() {
-  await updateVariantImage();
   return (
     <div className="p-5">
       <div className="w-100 flex justify-end gap-x-5">
